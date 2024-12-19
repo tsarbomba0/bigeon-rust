17cfd5f9c5530de7ed9d5ed1ee721d9dfaa0da63
tsarbomba0 <creepermaster000@gmail.com>
2024-12-19 22:55:39 +0000

    client

--- src/discord/client.rs
+++ src/discord/client.rs
@@ -1,4 +1,31 @@
+use crate::discord::message::{DiscordEmbed, DiscordError, DiscordMessage, MessageBuilder, Reply};
 use crate::https::https_client::Client;
 use crate::https::parse_http::parse_http;
 use crate::https::request::{Request, RequestBuilder};
 use crate::https::response::Response;
+
+pub struct DiscordClient {
+    conn: Client,
+    token: String,
+    base_headers: Vec<String>,
+}
+
+impl DiscordClient {
+    pub fn new(token: &str, headers: Option<&Vec<String>>) -> Self {
+        let base_headers = match headers {
+            Some(h) => h.to_owned(),
+            None => vec![],
+        };
+
+        Self {
+            token: token.to_owned(),
+            base_headers,
+            conn: Client::new("https://discord.com").unwrap(),
+        }
+    }
+    pub async fn send_message(&mut self, msg: DiscordMessage) -> Result<Box<dyn Reply>, Box<dyn std::error::Error>> {
+            let request = RequestBuilder::new()
+            .headers("Authorization:")
+            self.conn.client_write()
+    }
+}
--- src/discord/message.rs
+++ src/discord/message.rs
@@ -26,12 +26,54 @@ pub struct DiscordMessage {
     poll: Option<String>, // TODO
 }
 
+// Message Builder
+pub struct MessageBuilder {
+    content: Option<String>,
+    nonce: Option<String>,
+    tts: Option<bool>,
+    embeds: Option<Vec<String>>,
+    allowed_mentions: Option<Vec<String>>, // TODO
+    message_reference: Option<String>,     // Message reference,
+    components: Option<Vec<String>>,       // TODO
+    sticker_ids: Option<Vec<String>>,
+    attachments: Option<Vec<String>>, // TODO
+    flags: Option<u32>,
+    enforce_nonce: Option<bool>,
+    poll: Option<String>, // TODO
+}
+
+impl MessageBuilder {
+    pub fn new() -> Self {
+        Self {
+            content: None,
+            nonce: None,
+            tts: None,
+            embeds: None,
+            allowed_mentions: None,
+            message_reference: None,
+            components: None,
+            sticker_ids: None,
+            attachments: None,
+            flags: None,
+            enforce_nonce: None,
+            poll: None,
+        }
+    }
+
+    pub fn content(&mut self, c: &str) -> &mut Self {
+        self.content = Some(c.to_owned());
+        self
+    }
+}
+
+// Enum
 enum DiscordReply {
     DiscordMessage,
     DiscordError,
 }
 
-trait Reply {
+// Reply trait
+pub trait Reply {
     fn is_error(&self) -> bool;
     fn to_str(&self) -> Result<String, Box<dyn std::error::Error>>;
 }
@@ -57,10 +99,10 @@ impl Reply for DiscordError {
 }
 
 fn read_discord_reply(json: &str) -> Result<Box<dyn Reply>, Box<dyn std::error::Error>> {
-    let msg: DiscordMessage;
+    let msg: Box<dyn Reply>;
     let error: DiscordError;
     match serde_json::from_str::<DiscordMessage>(json) {
-        Ok(out) => msg = out,
+        Ok(out) => msg = Box::new(out),
         Err(e) => {
             if e.is_data() {
                 error = serde_json::from_str::<DiscordError>(json)?;
@@ -70,5 +112,5 @@ fn read_discord_reply(json: &str) -> Result<Box<dyn Reply>, Box<dyn std::error::
             }
         }
     };
-    Ok(Box::new(msg))
+    Ok(msg)
 }
